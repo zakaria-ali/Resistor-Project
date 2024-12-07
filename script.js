4727ec7a0ea5d0703472f313a9f58bb608f8cfc0
Zakaria WildAli <122523263+zakaria-ali@users.noreply.github.com>
2024-12-07 15:32:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ const multiplier = document.querySelector(".multiplier");
 const resistorValue = document.querySelector(".resistor-value");
 const minValue = document.querySelector(".min-value");
 const maxValue = document.querySelector(".max-value");
+const step1_1 = document.querySelector(".step1_1");
+const step1_2 = document.querySelector(".step1_2");
+const step2_1 = document.querySelector(".step2_1");
+const step2_2 = document.querySelector(".step2_2");
+const step3_1 = document.querySelector(".step3_1");
+const step3_2 = document.querySelector(".step3_2");
+const step3_3 = document.querySelector(".step3_3");
+const step4_1 = document.querySelector(".step4_1");
+const step4_2 = document.querySelector(".step4_2");
+const step4_3 = document.querySelector(".step4_3");
+
 let theNumberValue;
 let theResistorValue;
 
@@ -44,19 +55,35 @@ function setColor(bandId, color) {
 
   // Update the corresponding band value
   if (bandId === "band1") {
+    step1_1.innerHTML = `الخطوة 1.1: النظر الى الجدول ومعرفة قيمه اللون المختار`;
+    step1_2.innerHTML = `الخطوة 1.2: يصبح لدينا متغير اسمه الرقم وتكون قيمته حسب اللون المختار `;
+    
     theNumber.innerHTML = `الرقم = _ ${colorProps.number}`;
     theNumberValue = colorProps.number * 10;
+
   } else if (bandId === "band2") {
+    step2_1.innerHTML = `الخطوة 2.1: النظر الى الجدول ومعرفة قيمه اللون المختار`
+    step2_2.innerHTML = `الخطوة 2.2: هكذا تكتمل قيمة المتغير ويصبح كالاتي`
+
     theNumberValue += colorProps.number;
     theNumber.innerHTML = `الرقم = ${theNumberValue}`;
+
   } else if (bandId === "band3") {
+    step3_1.innerHTML = `الخطوة 3.1: النظر الى الجدول ومعرفة قيمه اللون المختار`
+    step3_2.innerHTML = `الخطوة 3.2: يتكون لدينا متغير جديد اسمه المضاعف وتكون قيمته كالاتي`
+    step3_3.innerHTML = `الخطوة 3.3: بعد معرفة قيمة الرقم والمضاعف اصبح بامكاننا حساب قيمة المقاومة حسب القانون ادناه`
+
     theResistorValue = theNumberValue * colorProps.multiplier;
     multiplier.innerHTML = `المضاعف = ${colorProps.multiplier}`;
     resistorValue.innerHTML = `${theResistorValue}Ω = ${colorProps.multiplier} * ${theNumberValue} = قيمة المقاومة = الرقم * المضاعف `;
+  
   } else if (bandId === "band4") {
+    step4_1.innerHTML = `الخطوة 4.1: النظر الى الجدول ومعرفة قيمه اللون المختار حيث يعبر عن نسبة الخطأ في قيمة المقاومة`
+    step4_2.innerHTML = `الخطوة 4.2: اللون الاخير عبارة عن نسبة الخطأ ومنه نحسب الحد الادنى لقيمة المقاومة`
+    step4_3.innerHTML = `الخطوة 4.3: بنفس الطريقة نحسب الحد الاعلى لقيمة المقاومة`
+
     minValue.innerHTML = `${theResistorValue - theResistorValue * colorProps.varianceRatio }Ω = ${colorProps.varianceRatio} * ${theResistorValue} - ${theResistorValue} = الحد الادنى للمقاومة = قيمة المقاومة - قيمة * (نسبة الخطأ)`;
     maxValue.innerHTML = `${theResistorValue + theResistorValue * colorProps.varianceRatio }Ω = ${colorProps.varianceRatio} * ${theResistorValue} + ${theResistorValue} = الحد الاعلى للمقاومة = قيمة المقاومة + قيمة * (نسبة الخطأ)`;
-
   }
 }
 
@@ -76,6 +103,17 @@ function resetValues() {
   resistorValue.innerHTML = "";
   minValue.innerHTML = "";
   maxValue.innerHTML = "";
+  step1_1.innerHTML = "";
+  step1_2.innerHTML = "";
+  step2_1.innerHTML = "";
+  step2_2.innerHTML = "";
+  step3_1.innerHTML = "";
+  step3_2.innerHTML = "";
+  step3_3.innerHTML = "";
+  step4_1.innerHTML = "";
+  step4_2.innerHTML = "";
+  step4_3.innerHTML = "";
+
 
   // Reset internal variables
   theNumberValue = undefined;
